55d2dbc65ccf3152c41ff7545a32ca06c64c0366
Forest Sharp <forestcollinssharp@gmail.com>
2021-03-31 06:14:31 -0500

    Sort Properties by categories in ImGuiObject

--- Source/UnrealImGui/Private/ImGuiFunctionLibrary.cpp
+++ Source/UnrealImGui/Private/ImGuiFunctionLibrary.cpp
@@ -49,119 +49,154 @@ void UImGuiFunctionLibrary::ImguiObject(UObject* InObject, const bool bOpenInNew
 {
 	if (ImGui::GetCurrentContext() && InObject != nullptr)
 	{
+		ImGui::PushID(InObject->GetUniqueID());
+
 		if (bOpenInNewWindow) { ImGui::Begin(TCHAR_TO_ANSI(*InObject->GetName())); }
-		
-		//FCS TODO: Sort by categories (use collapsing headers)
+
+		const float Indentation = 16.0f;
+
+		//Display SubObjects first
+		if (ImGui::CollapsingHeader("SubObjects"))
 		{
-			int32 PropertyIndex = 0;
+			ImGui::Indent(Indentation);
+			for (FObjectProperty* ObjectProperty : TFieldRange<FObjectProperty>(InObject->GetClass()))
+			{
+				const char* PropertyName = TCHAR_TO_ANSI(*ObjectProperty->GetName());
+				void* PropertyAddress = ObjectProperty->ContainerPtrToValuePtr<void>(InObject);
+			
+				UObject* ChildObject = ObjectProperty->GetObjectPropertyValue(PropertyAddress);
+				if (ChildObject != nullptr)
+				{
+					if (ImGui::CollapsingHeader(PropertyName))
+					{
+						ImGui::Indent(Indentation);
+						ImguiObject(ChildObject, false);
+						ImGui::Unindent(Indentation);
+					}
+				}
+			}
+			ImGui::Unindent(Indentation);
+		}
+
+		ImGui::Separator();
+
+		if (ImGui::CollapsingHeader("Properties"))
+		{
+			//FCS TODO: Sort by Categories (use collapsing headers) (one Table per Category)
+			TMap<FString, TArray<FProperty*>> Categories;
 			for (FProperty* Property : TFieldRange<FProperty>(InObject->GetClass()))
 			{
 				//Ignored Properties
 				if (   Property->IsA<FDelegateProperty>()
-					|| Property->IsA<FMulticastDelegateProperty>()
-					|| Property->IsA<FMulticastInlineDelegateProperty>()
-					|| Property->IsA<FMulticastSparseDelegateProperty>()
-					|| Property->IsA<FInterfaceProperty>()
-					//TODO: Ignore FLazyObjectProperty, FSoftObjectProperty, FClassProperty, FSoftClassProperty
-					)
+                    || Property->IsA<FMulticastDelegateProperty>()
+                    || Property->IsA<FMulticastInlineDelegateProperty>()
+                    || Property->IsA<FMulticastSparseDelegateProperty>()
+                    || Property->IsA<FInterfaceProperty>()
+                    || Property->IsA<FObjectProperty>() //Handled Above
+                    //TODO: Ignore FLazyObjectProperty, FSoftObjectProperty, FClassProperty, FSoftClassProperty
+                    )
 				{
 					continue;
 				}
-				
-				FString IDString = FString::FromInt(InObject->GetUniqueID()) + TEXT("_") + FString::FromInt(PropertyIndex++);
-				ImGui::PushID(TCHAR_TO_ANSI(*IDString));
 
-				const char* PropertyName = TCHAR_TO_ANSI(*Property->GetName());
-				void* PropertyAddress = Property->ContainerPtrToValuePtr<void>(InObject);
-
-				//TODO: FStructProperty, FSetProperty, FMapProperty, FArrayProperty, FWeakObjectProperty, FNameProperty, FStrProperty
+				//FCS TODO: If Runtime, Don't show "EditDefaultsOnly" properties
+				//FCS TODO: Option to only show "EditAnywhere"
 				
-				if (FObjectProperty* ObjectProperty = CastField<FObjectProperty>(Property))
+				const FString& Category = Property->GetMetaData(TEXT("Category"));
+				Categories.FindOrAdd(Category).Add(Property);
+			}
+
+			for (TTuple<FString, TArray<FProperty*>> Category : Categories)
+			{
+				const FString CategoryName = Category.Key.IsEmpty() ? TEXT("Uncategorized") : Category.Key;
+				
+				if (Category.Value.Num() > 0 && ImGui::CollapsingHeader(TCHAR_TO_ANSI(*CategoryName)))
 				{
-					UObject* ChildObject = ObjectProperty->GetObjectPropertyValue(PropertyAddress);
-					if (ChildObject != nullptr)
+					ImGui::Indent(Indentation);
+					if (ImGui::BeginTable("split", 2, ImGuiTableFlags_Resizable))
 					{
-						const float Indentation = 16.0f;
-						if (ImGui::CollapsingHeader(PropertyName))
+						for (FProperty* Property : Category.Value)
 						{
-							ImGui::Indent(Indentation);
-							ImguiObject(ChildObject, false);
-							ImGui::Unindent(Indentation);
-						}
-					}
-				}
-				else if (ImGui::BeginTable("split", 2, ImGuiTableFlags_Resizable))
-				{
-					ImGui::TableNextRow();
+							const char* PropertyName = TCHAR_TO_ANSI(*Property->GetName());
+							void* PropertyAddress = Property->ContainerPtrToValuePtr<void>(InObject);
+
+							ImGui::PushID(PropertyAddress);
+
+							//TODO: FStructProperty, FSetProperty, FMapProperty, FArrayProperty, FWeakObjectProperty, FNameProperty, FStrProperty
 					
-					ImGui::TableSetColumnIndex(0);
-					{
+							ImGui::TableNextRow();
 						
-						ImGui::AlignTextToFramePadding();
-						ImGui::Text(PropertyName);
-					}
-
-	                ImGui::TableSetColumnIndex(1);
-					{
-						if (FFloatProperty* FloatProperty = CastField<FFloatProperty>(Property))
-						{
-							float* FloatPtr = FloatProperty->GetPropertyValuePtr(PropertyAddress);
-							ImGui::DragFloat("##FloatValue", FloatPtr);
-							//TODO: use SliderMin/Max if available, use ClampMin/Max if available
-						}
-						else if (FIntProperty* IntProperty = CastField<FIntProperty>(Property))
-						{
-							int32* IntPtr = IntProperty->GetPropertyValuePtr(PropertyAddress);
-							ImGui::DragInt("##IntValue", IntPtr);
-							//TODO: use SliderMin/Max if available, use ClampMin/Max if available
-						}
-						else if (FBoolProperty* BoolProperty = CastField<FBoolProperty>(Property))
-						{
-							bool BoolValue = BoolProperty->GetPropertyValue(PropertyAddress);
-							if (ImGui::Checkbox("##BoolValue", &BoolValue))
+							ImGui::TableSetColumnIndex(0);
 							{
-								BoolProperty->SetPropertyValue(PropertyAddress, BoolValue);
+							
+								ImGui::AlignTextToFramePadding();
+								ImGui::Text(PropertyName);
 							}
-						}
-						else if (FEnumProperty* EnumProperty = CastField<FEnumProperty>(Property))
-						{
-							FNumericProperty* UnderlyingProperty = EnumProperty->GetUnderlyingProperty();
-							UEnum* Enum = EnumProperty->GetEnum();
-							if (UnderlyingProperty && Enum)
+
+							ImGui::TableSetColumnIndex(1);
 							{
-								const int64 CurrentValue = UnderlyingProperty->GetSignedIntPropertyValue(PropertyAddress);
-								const int32 CurrentIndex = Enum->GetIndexByValue(CurrentValue);
-								FString CurrentValueName = Enum->GetDisplayNameTextByIndex(CurrentIndex).ToString();
-						
-								if (ImGui::BeginCombo("##EnumCombo", TCHAR_TO_ANSI(*CurrentValueName)))
+								if (FFloatProperty* FloatProperty = CastField<FFloatProperty>(Property))
 								{
-									//FCS TODO: Don't display the "MAX" entry
-									for (int32 i = 0; i < Enum->NumEnums(); ++i)
+									float* FloatPtr = FloatProperty->GetPropertyValuePtr(PropertyAddress);
+									ImGui::DragFloat("##FloatValue", FloatPtr);
+									//TODO: use SliderMin/Max if available, use ClampMin/Max if available
+								}
+								else if (FIntProperty* IntProperty = CastField<FIntProperty>(Property))
+								{
+									int32* IntPtr = IntProperty->GetPropertyValuePtr(PropertyAddress);
+									ImGui::DragInt("##IntValue", IntPtr);
+									//TODO: use SliderMin/Max if available, use ClampMin/Max if available
+								}
+								else if (FBoolProperty* BoolProperty = CastField<FBoolProperty>(Property))
+								{
+									bool BoolValue = BoolProperty->GetPropertyValue(PropertyAddress);
+									if (ImGui::Checkbox("##BoolValue", &BoolValue))
 									{
-										const bool bIsSelected = i == CurrentIndex;
-										if (ImGui::Selectable(TCHAR_TO_ANSI(*Enum->GetDisplayNameTextByIndex(i).ToString()), bIsSelected))
+										BoolProperty->SetPropertyValue(PropertyAddress, BoolValue);
+									}
+								}
+								else if (FEnumProperty* EnumProperty = CastField<FEnumProperty>(Property))
+								{
+									FNumericProperty* UnderlyingProperty = EnumProperty->GetUnderlyingProperty();
+									UEnum* Enum = EnumProperty->GetEnum();
+									if (UnderlyingProperty && Enum)
+									{
+										const int64 CurrentValue = UnderlyingProperty->GetSignedIntPropertyValue(PropertyAddress);
+										const int32 CurrentIndex = Enum->GetIndexByValue(CurrentValue);
+										FString CurrentValueName = Enum->GetDisplayNameTextByIndex(CurrentIndex).ToString();
+							
+										if (ImGui::BeginCombo("##EnumCombo", TCHAR_TO_ANSI(*CurrentValueName)))
 										{
-											const int64 NewValue = Enum->GetValueByIndex(i);
-											UnderlyingProperty->SetIntPropertyValue(PropertyAddress, NewValue);
-										}  
-										if (bIsSelected)
-										{
-											// Set the initial focus when opening the combo
-											ImGui::SetItemDefaultFocus();
+											//FCS TODO: Don't display the "MAX" entry
+											for (int32 i = 0; i < Enum->NumEnums(); ++i)
+											{
+												const bool bIsSelected = i == CurrentIndex;
+												if (ImGui::Selectable(TCHAR_TO_ANSI(*Enum->GetDisplayNameTextByIndex(i).ToString()), bIsSelected))
+												{
+													const int64 NewValue = Enum->GetValueByIndex(i);
+													UnderlyingProperty->SetIntPropertyValue(PropertyAddress, NewValue);
+												}  
+												if (bIsSelected)
+												{
+													// Set the initial focus when opening the combo
+													ImGui::SetItemDefaultFocus();
+												}
+											}
+											ImGui::EndCombo();
 										}
 									}
-									ImGui::EndCombo();
 								}
 							}
+							ImGui::PopID();
 						}
+						ImGui::EndTable();
 					}
-					ImGui::EndTable();
+					ImGui::Unindent(Indentation);
 				}
-
-				ImGui::PopID();
 			}
 		}
 
+		ImGui::PopID();
 		if (bOpenInNewWindow) { ImGui::End(); }
 	}
 }
